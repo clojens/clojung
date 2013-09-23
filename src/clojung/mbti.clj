a398c9e7214428b7e251ab99960a093dca366ba6
supersymmetry <maildumpert@gmail.com>
2013-09-23 09:48:05 +0200

    cleaned up a bit + fixes mbti and added keirsey

--- src/clojung/mbti.clj
+++ src/clojung/mbti.clj
@@ -1,22 +1,30 @@
-(ns clojung.mbti
+(ns
+
+  ^{:doc "Contains a derived model for psychological preferences based on the
+    work of Carl Gustav Jung known as the Meyers-Briggs Type Indicator (MBTI).
+    It predicts to a certain degree in how people perceive the world and make
+    decisions based on those preferences."}
+
+  clojung.mbti
+
   (:require [clojure.math.combinatorics :refer [cartesian-product]]
             [plumbing.core :refer :all]
             [plumbing.graph :as graph]))
 
-;;
-;; psychological preferences in how people perceive the world & make decisions
-;;
+(defn list-all-types
+  "Returns a sorted list of these psychological differences into four opposite
+  pairs, or dichotomies, with a resulting 16 possible psychological types."
+  []
+  (map #(apply str %)
+       (cartesian-product [\I \E] [\S \N] [\F \T] [\P \J])))
 
+;; A bipartition, two parts (subsets) that are jointly exhaustive (everything
+;; must belong to one part or the other) and mutually exclusive (nothing can
+;; belong simultaneously to both parts).
 
-;; The MBTI sorts some of these psychological differences into four opposite pairs, or dichotomies, with a resulting 16 possible psychological types.
-(def all-types (map #(apply str %) (cartesian-product [\I \E] [\S \N]
-                                                      [\F \T] [\P \J]))) ; 16x
+;; NOTE: While in logic, the partitions are opposites if there exists a proposition
+;; such that it holds over one and not the other. In MBTI this indicates a preference.
 
-;; A bipartition, two parts (subsets) that are:
-;; jointly exhaustive: everything must belong to one part or the other, and
-;; mutually exclusive: nothing can belong simultaneously to both parts.
-;; In logic, the partitions are opposites if there exists a proposition such
-;; that it holds over one and not the other. In MBTI this indicates a preference.
 (def dichotomies
   {
    ;; Extraversion (E)	–	(I) Introversion
@@ -26,34 +34,26 @@
    ;; internal world of ideas and reflection ("introverted attitude").
    \E \I \I \E
 
-   ;; Four (4) principal psychological functions
-   ;; --------------------------------------------------------
-   ;; Sensing (S)	–	(N) Intuition
-   ;; The "irrational" (perceiving) functions.
+   ;; The four (4) principal psychological functions (S-N and T-F):
+
+   ;; Sensing  (S)	–	(N) Intuition (the "irrational" (perceiving) functions)
    \S \N \N \S
 
-   ;; Thinking (T)	–	(F) Feeling
-   ;; The "rational" (judging) functions.
+   ;; Thinking (T)	–	(F) Feeling (the "rational" (judging) functions)
    \T \F \F \T
-   ;; --------------------------------------------------------
 
-   ;; Judging (J) - (P) Perception
-   ;;
-   ;; Lifestyle is the final dimension additional to Jung's model,
-   ;; denotes preference for using either the judging function (thinking
-   ;; or feeling) or their perceiving function (sensing or intuition)
-   ;; when relating to the outside world (extraversion).
+   ;; Judging (J) - (P) Perception (preference using either J or P extraverted)
    \J \P \P \J})
 
-
-(defn toggle
-  "Takes any key and a value supposedly in the dichotomies and
-  returns a map with that key and its complement."
-  [k v] (update-in {k v} [k] dichotomies))
+;; (defn toggle
+;;   "Takes any key and a value supposedly in the dichotomies and
+;;   returns a map with that key and its complement. You can also
+;;   just use e.g. (dichotomies \T) and it will return `\F`"
+;;   [k v] (update-in {k v} [k] dichotomies))
 
 
 ;;;
-;;; Keyword-function definitions
+;;; J/P Rational/Irrational functions
 ;;;
 
 (defnk perceiving-fn [abbr] (nth abbr 1)) ; irrational S-N
@@ -64,31 +64,34 @@
 ;;;
 
 (defnk attitude
-  "The attitude preference (E-I) determines whether the extraverted function is dominant or auxiliary."
+  "The attitude preference (E-I) determines whether the extraverted function is
+  dominant or auxiliary."
   [abbr] (first abbr))
 
 (defnk lifestyle
-  "The overall lifestyle preference (J-P) determines whether the judging (T-F) or perceiving (S-N)
-  preference is most evident in the outside world; i.e., which function has an extraverted attitude."
+  "The overall lifestyle preference (J-P) determines whether the judging (T-F) or
+  perceiving (S-N) preference is most evident in the outside world; i.e., which
+  function has an extraverted attitude."
   [abbr] (last abbr))
 
-(defnk prefered-fn [abbr lifestyle] (:p (toggle :p lifestyle)))
+;;?
+(defnk prefered-fn
+  "Flipped value to determine preference based on lifestyle."
+  [abbr lifestyle] (:p (toggle :p lifestyle)))
 
 (defnk extraverted-fn
-  "Function paired with the extraverted attitude: dominant for those with
-  preference for extraverted attitude, auxiliary for those who prefer
-  introversion."
+  "Function paired with the extraverted attitude: dominant for those who prefer
+  extraverted attitude, auxiliary for those that preference introversion."
   [abbr p?] (if p? (second abbr) (nth abbr 2)))
 
 (defnk introverted-fn
-  "Function paired the introverted attitude: dominant for those with
-  preference for extraverted attitude, auxiliary for those who prefer
-  extraversion."
+  "Function paired the introverted attitude: dominant for those with preference
+  for extraverted attitude, auxiliary for those who prefer extraversion."
   [extraverted perceiving-fn judging-fn]
   (if (= extraverted perceiving-fn) judging-fn perceiving-fn))
 
 ;;;
-;;; 4 functions
+;;; Cognitive functions
 ;;;
 
 (defnk dominant-fn
@@ -134,28 +137,38 @@
 
 (def mbti-graph
   {:attitude attitude
-   :perceiving-fn perceiving-fn
-   :judging-fn judging-fn
-   :lifestyle lifestyle
+   :perceiving-fn perceiving-fn ; how information is taken in
+   :judging-fn judging-fn ; how decissions are made
+   :lifestyle lifestyle ; external (world) orientation
    :prefered-fn prefered-fn
    :extraverted extraverted-fn
    :introverted introverted-fn
 
-   ;; Predicates placed here due to breivity
+   ;; Predicates inner fn due to breivity
+
+   ;; Predicts how types will focus their attention or get their energy.
    :i? (fnk [attitude] (= attitude \I))
    :e? (fnk [attitude] (= attitude \E))
-   :p? (fnk [lifestyle] (= lifestyle \P))
-   :j? (fnk [lifestyle] (= lifestyle \J))
-   :t? (fnk [judging-fn] (= judging-fn \T))
-   :f? (fnk [judging-fn] (= judging-fn \F))
+
+   ;; Predicts how a type will perceive or take in information.
    :s? (fnk [perceiving-fn] (= perceiving-fn \S))
    :n? (fnk [perceiving-fn] (= perceiving-fn \N))
+
+   ;; Predicts how a type will prefer to make decisions.
+   :p? (fnk [lifestyle] (= lifestyle \P))
+   :j? (fnk [lifestyle] (= lifestyle \J))
+
+   ;; Predicts how a type will orient themselves to the external world.
+   :t? (fnk [judging-fn] (= judging-fn \T))
+   :f? (fnk [judging-fn] (= judging-fn \F))
+
    :rational? (fnk [prefered-fn] (= prefered-fn \J))
    :irrational? (fnk [prefered-fn] (= prefered-fn \P))
 
+   ;; Keyword emitting functions for clarity mostly
    :ratio (fnk [rational?] (if rational? :rational :irrational))
 
-   ;; The interaction of two, three, or four preferences is
+   ;; The interaction of 2, 3, or 4 preferences is
    ;; known as type dynamics.
 
    ;; Is likely to be evident earliest in life.
@@ -179,6 +192,19 @@
 (->> (eager-mbti {:abbr "ENFP"})
      ((juxt :dominant :auxiliary :tertiary :inferior :ratio)))
 
+(defn keirsey-temperaments
+  [[first second third fourth :as abbr]]
+  (cond
+   (and (= second \S)
+        (= fourth \J)) "Guardian"
+   (and (= second \S)
+        (= fourth \P)) "Artisan"
+   (and (= second \N)
+        (= third  \F)) "Idealist"
+   (and (= second \N)
+        (= third  \T)) "Rational"))
+
+(keirsey-temperaments "INTP")
 
 
 
